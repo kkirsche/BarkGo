9f59ba201f6a38579e52143b96f14d10da8fb6a1
Kevin Kirsche <kev.kirsche@gmail.com>
2015-10-15 15:53:53 -0400

    Get the initial XML parsing working.

--- gobark.go
+++ gobark.go
@@ -36,14 +36,25 @@ func CreateClient(protocol, user, password, subdomain, host, port string) BarkCl
   return *client
 }
 
-func toUtf8(iso8859_1_buf []byte) string {
-    buf := make([]rune, len(iso8859_1_buf))
-    for i, b := range iso8859_1_buf {
-        buf[i] = rune(b)
+func GetMonitStatus(url string) string {
+  response, err := http.Get(url)
+  if err != nil {
+    fmt.Printf("%s", err)
+    os.Exit(1)
+  } else {
+    defer response.Body.Close()
+    contents, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+      fmt.Printf("%s", err)
+      os.Exit(1)
     }
-    return string(buf)
-}
 
+    iso_string_xml := string(contents[:])
+    utf_string_xml := strings.Replace(iso_string_xml, "ISO-8859-1", "utf-8", -1)
+    return utf_string_xml
+  }
+  return "oops"
+}
 func main() {
   var buffer bytes.Buffer
 
@@ -69,12 +80,17 @@ func main() {
       os.Exit(1)
     }
 
-    utf_string_xml := toUtf8(contents)
+    iso_string_xml := string(contents[:])
+    utf_string_xml := strings.Replace(iso_string_xml, "ISO-8859-1", "utf-8", -1)
+
     node, err := xmlpath.Parse(strings.NewReader(utf_string_xml))
     if err != nil {
       fmt.Println(err)
     }
 
-    fmt.Println(node)
+    target_path := xmlpath.MustCompile("/monit/server/version")
+    if value, ok := target_path.String(node); ok {
+      fmt.Println("Found:", value)
+    }
   }
 }
